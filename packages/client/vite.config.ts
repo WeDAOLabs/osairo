9c8a39f1ee736890c2525be9aa5c31caabba1ee2
enixlee <405284695@qq.com>
2023-11-15 20:24:47 +0800

    add vite config

---
+++ packages/client/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  server: {
+    port: 3000,
+    fs: {
+      strict: false,
+    },
+  },
+  build: {
+    target: "es2022",
+    minify: true,
+    sourcemap: true,
+    rollupOptions: {
+      input: "src/index.ts",
+    },
+    outDir: "assets/scripts/mud/",
+    assetsDir: ".",
+  },
+});
